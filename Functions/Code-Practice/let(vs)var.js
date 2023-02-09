3c98883e8163608fc9f52f54e7cfb10a9c381557
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-02-09 17:53:00 +0530

    Function Params

--- Functions/Code-Practice/let(vs)var.js
+++ Functions/Code-Practice/let(vs)var.js
@@ -16,3 +16,10 @@ incement();
 print(count);   
 
 // print = 3; FIXME: Read about TypeError: Assignment to the Constant Variable 
+
+/**
+ * TODO: If we pass arguments to any Function, That Function will make a copy of that variable
+ * TODO: and It takes as a parameter. and It's gonna be the local copy of that global argument.
+ * TODO: So that we can't modify the Global variable. It's a good practise to pass an argument and take in 
+ * TODO: as parameter and creates a new copy in the LOCAL EXECUTION CONTEXT in the CALLSTACK.
+ */
