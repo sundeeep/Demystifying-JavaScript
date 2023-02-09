effa6f8e495614a9bcde36b182c01154dfff1378
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-02-09 17:48:22 +0530

    let vs var updated!

--- Functions/Code-Practice/let(vs)var.js
+++ Functions/Code-Practice/let(vs)var.js
@@ -1,14 +1,18 @@
-var print = (prompt) => {
+const print = (prompt) => {
     console.log(prompt);
 }
 
 let count = 0;
 
 function incement(){
+    // let count = counter;
+    let name = "Sandeep";
     count += 1;
     print(count);
 }
 
-print(count);
-incement();
-print(count);
+print(count);     
+incement();      
+print(count);   
+
+// print = 3; FIXME: Read about TypeError: Assignment to the Constant Variable 
