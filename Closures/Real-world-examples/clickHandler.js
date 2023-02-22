991ea3b1c006e91698d5e7e67094cbd0444acc1b
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-02-22 13:27:57 +0530

    Closures : onClick Event

--- Closures/Real-world-examples/clickHandler.js
+++ Closures/Real-world-examples/clickHandler.js
@@ -1,5 +1,35 @@
 const changeBG = document.querySelector(".ChangeBG");
 
-changeBG.addEventListener("click", ()=>{
-    document.body.style.backgroundColor = "green";
-})
+// changeBG.addEventListener("", ()=>{
+//     document.body.style.backgroundColor = "green"; 
+// }); //This will run perfectly because we are giving the call back function to the `addEventListener` which takes care of runninf this call back function.
+ //By the way, addEventListener is the Higher Ordeer Function, which accepts two parameters, 1. Event and 2. Call Back Function.
+
+
+// changeBG.addEventListener("click", changeBGHandler()); //already invoked, so return the function instead of returning the logic on function invocation.
+
+// function changeBGHandler(){
+//     document.body.style.backgroundColor = "green";
+// }
+
+// TODO: Use Closures to do the Production Level Stuff in this onClick Handlers.
+
+// changeBG.addEventListener('click', changeBGHandler("yellow"));
+
+
+// TODO: Switch the Button Behaviour
+// let BGChanged = false;
+// function changeBGHandler(){
+
+//     if(BGChanged){
+//         document.body.style.backgroundColor = "";
+//         changeBG.innerText = "Change the BG of Body";
+//         BGChanged = false;
+//         return;
+//     }
+
+//     document.body.style.backgroundColor = 'lightcoral';
+//     changeBG.innerText = "Revert Back";
+//     BGChanged = true;
+
+// }
